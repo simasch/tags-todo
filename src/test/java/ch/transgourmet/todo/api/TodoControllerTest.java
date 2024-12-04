6ca8c93b444539fc99d3caffaa7a51f2baa49a56
Simon Martinelli <simon@martinelli.ch>
2024-12-04 14:00:30 +0100

    Use var

--- src/test/java/ch/transgourmet/todo/api/TodoControllerTest.java
+++ src/test/java/ch/transgourmet/todo/api/TodoControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -35,11 +34,11 @@ class TodoControllerTest {
                                 }"""))
                 .andExpect(status().isCreated());
 
-        MvcResult mvcResult = mockMvc.perform(get("/todos"))
+        var mvcResult = mockMvc.perform(get("/todos"))
                 .andExpect(status().isOk())
                 .andReturn();
 
-        TodoDTO[] todos = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), TodoDTO[].class);
+        var todos = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), TodoDTO[].class);
         assertThat(todos).hasSize(2);
 
         mockMvc.perform(delete("/todos/1"))
